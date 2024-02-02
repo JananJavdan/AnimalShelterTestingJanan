53c9158083aa2a3439c718730e9b746e438a70c6
Janan <fatemeh.javdan@student.intecbrussel.be>
2024-02-02 14:52:45 +0100

    Add files

--- src/test/java/be/intecbrussel/AnimalShelterTest/AnimalShelterTest.java
+++ src/test/java/be/intecbrussel/AnimalShelterTest/AnimalShelterTest.java
@@ -16,6 +16,10 @@ class AnimalShelterTest {
     {
         animalShelter = new AnimalShelter();
     }
+    private final AnimalShelter1 animalShelter1;
+    {
+        animalShelter1 = new AnimalShelter1();
+    }
 
 
 
@@ -39,41 +43,45 @@ class AnimalShelterTest {
     @Test
     void sortAnimalsByNameTest() {
         Animal dog1 = new Dog(false, 3, "Noel", 123, true);
-        Animal cat1 = new Cat(false, 3, "Mitzi", 345, true);
-        Animal cat2 = new Cat(false, 2, "KittY", 376, true);
+        Animal dog2 = new Dog(false, 4, "Louie", 222, true);
+        Animal dog3 = new Dog(false, 2, "Kobey", 33, true);
 
         animalShelter.addAnimal(dog1);
-        animalShelter.addAnimal(cat1);
-        animalShelter.addAnimal(cat2);
+        animalShelter.addAnimal(dog2);
+        animalShelter.addAnimal(dog3);
 
         animalShelter.sortAnimalByName();
 
-        assertEquals(cat1, animalShelter.findAnimal(2).orElse(null));
-        assertEquals(dog1, animalShelter.findAnimal(1).orElse(null));
-        assertEquals(cat2, animalShelter.findAnimal(3).orElse(null));
+        assertEquals(dog3, animalShelter.findAnimal("Kobey").orElse(null));
+        assertEquals(dog2, animalShelter.findAnimal("Louie").orElse(null));
+        assertEquals(dog1, animalShelter.findAnimal("Noel").orElse(null));
     }
 
     @Test
     void SrtAnimalsByAgeTest() {
-        Animal cat1 = new Dog(false, 3, "Mitzi", 345, true);
-        Animal cat2 = new Cat(false, 2, "KittY", 376, true);
-        Animal cat3 = new Cat(false, 1, "Lola", 398, true);
+        Animal dog1 = new Dog(false, 3, "Noel", 123, true);
+        Animal dog2 = new Dog(false, 4, "Louie", 222, true);
+        Animal dog3 = new Dog(false, 2, "Kobey", 33, true);
 
-        animalShelter.addAnimal(cat1);
-        animalShelter.addAnimal(cat2);
-        animalShelter.addAnimal(cat3);
+        animalShelter.addAnimal(dog1);
+        animalShelter.addAnimal(dog2);
+        animalShelter.addAnimal(dog3);
 
         animalShelter.sortAnimalByAge();
 
-        assertEquals(cat1, animalShelter.findAnimal(3).orElse(null));
-        assertEquals(cat2, animalShelter.findAnimal(2).orElse(null));
-        assertEquals(cat3, animalShelter.findAnimal(1).orElse(null));
+        assertEquals(dog3, animalShelter.findAnimal(33).orElse(null));
+        assertEquals(dog2, animalShelter.findAnimal(222).orElse(null));
+        assertEquals(dog1, animalShelter.findAnimal(123).orElse(null));
+
+
     }
+
+
 
     @Test
     void printAnimalsNotVaccinatedTest() {
-        Animal dog = new Dog(false, 3, "Noel", 123, true);
-        Animal cat = new Cat(false, 2, "KittY", 376, true);
+        Animal dog = new Dog(false, 5, "Charlie", 1, true);
+        Animal cat = new Cat(false, 3, "Buddy", 2, false);
 
         animalShelter.addAnimal(dog);
         animalShelter.addAnimal(cat);
@@ -85,11 +93,12 @@ class AnimalShelterTest {
         animalShelter.printAnimalsNotVaccinated(Disease.POLIO);
 
         String expectedOutput = "***************** Animals not vaccinated for POLIO ********************\n" +
-                "isClean=false, age=3, name='Noel', animalNumber=123\n" +
-                "isClean=false, age=2, name='Variable...', animalNumber=376\n";
+                "isClean=false, age=5, name='Charlie', animalNumber=1\n" +
+                "isClean=false, age=3, name='Buddy', animalNumber=2\n";
 
         assertEquals(expectedOutput.trim(), outPut.toString().trim());
     }
+
 
     @Test
     void printAnimalsVaccinatedTest() {
@@ -115,28 +124,30 @@ class AnimalShelterTest {
 
     @Test
     void findAnimalByNumberTest() {
-        Animal monkey1 = new Monkey(false, 11, "Kimmy", 213, true);
-        Animal monkey2 = new Monkey(false, 15, "Jolly", 331, true);
-        Animal monkey3 = new Monkey(false, 10, "Perry", 453, true);
 
-        animalShelter.addAnimal(monkey1);
-        animalShelter.addAnimal(monkey2);
-        animalShelter.addAnimal(monkey3);
+        Monkey jon = new Monkey(true, 28, "Jonathan", 28, true);
+        Monkey mary = new Monkey(false, 29, "mary", 29, true);
+        Monkey leen = new Monkey(true, 27, "leen", 27, false);
+        Monkey martin = new Monkey(false, 26, "martin", 26, false);
+
+        animalShelter.addAnimal(jon);
+        animalShelter.addAnimal(mary);
+        animalShelter.addAnimal(leen);
+        animalShelter.addAnimal(martin);
+
+        animalShelter1.findAnimal(29);
+
+        int existingAnimalNumber = 29;
+        assertTrue(animalShelter.findAnimal(existingAnimalNumber).isPresent(),
+                "Het dier met animalNumber " + existingAnimalNumber + " moet gevonden worden.");
+
+        int notExistingAnimalNumber = 999;
+        assertFalse(animalShelter.findAnimal(notExistingAnimalNumber).isPresent(),
+                "Het dier met animalNumber" + notExistingAnimalNumber+ " mag niet gevonden worden");
 
 
-        Optional<Animal> foundAnimal3 = animalShelter.findAnimal(453);
-        Optional<Animal> foundAnimal2 = animalShelter.findAnimal(331);
-        Optional<Animal> foundAnimal1 = animalShelter.findAnimal(213);
-
-        assertTrue(foundAnimal3.isPresent());
-        assertEquals(monkey3, foundAnimal3.get());
-
-        assertTrue(foundAnimal2.isPresent());
-        assertEquals(monkey2, foundAnimal2.get());
-
-        assertTrue(foundAnimal1.isPresent());
-        assertEquals(monkey1, foundAnimal1.get());
     }
+
 
 
     @Test
@@ -150,103 +161,129 @@ class AnimalShelterTest {
         animalShelter.addAnimal(monkey2);
         animalShelter.addAnimal(monkey3);
 
-        Optional<Animal> foundAnimal = animalShelter.findAnimal(453);
+        Optional<Animal> foundAnimal = animalShelter.findAnimal("Perry");
 
         assertTrue(foundAnimal.isPresent());
-        assertEquals("Cheeky", foundAnimal.get().getName());
+        assertEquals("Perry", foundAnimal.get().getName());
+
+        String existingAnimalName = "Perry";
+        assertTrue(animalShelter.findAnimal(existingAnimalName).isPresent(),
+                "Het dier met animalName " + existingAnimalName + " moet gevonden worden.");
+
+        String notExistingAnimalName = "Max";
+        assertFalse(animalShelter.findAnimal(notExistingAnimalName).isPresent(),
+        "Het dier met animalName" + notExistingAnimalName+ " mag niet gevonden worden");
+
     }
 
 
 
     @Test
     void FindAnimalByNameTest() {
-        Animal dog = new Dog(false, 2, "Kobey", 33, true);
-        Animal cat = new Cat(false, 7, "Nali", 798, true);
+        AnimalShelter1 animalShelter1 = new AnimalShelter1();
+        Monkey jon = new Monkey(true, 28, "Jonathan", 28, true);
+        Monkey mary = new Monkey(false, 29, "mary", 29, true);
+        Monkey leen = new Monkey(true, 27, "leen", 27, false);
+        Monkey martin = new Monkey(false, 26, "martin", 26, false);
 
-        animalShelter.addAnimal(dog);
-        animalShelter.addAnimal(cat);
+        animalShelter1.addAnimal(jon);
+        animalShelter1.addAnimal(mary);
+        animalShelter1.addAnimal(leen);
+        animalShelter1.addAnimal(martin);
 
-        Optional<Animal> foundAnimal = animalShelter.findAnimal("Kimmy");
+        animalShelter1.treatAnimal("martin");
 
-        assertTrue(foundAnimal.isPresent());
-        assertEquals(cat, foundAnimal.get());
+        Animal treatedAnimal = animalShelter1.findAnimal("martin");
+        boolean isCleanAfterTreatment = treatedAnimal != null && treatedAnimal.isClean();
+        assertEquals(true, isCleanAfterTreatment, "Het dier met ANIMALNAME martin moet zijn behandeld en schoon zijn.");
     }
+
 
     @Test
     void treatAnimalByNumberTest() {
-        Animal monkey = new Monkey(true, 28, "Jonathan", 28, true);
+        AnimalShelter1 animalShelter1 = new AnimalShelter1();
+        Monkey jon = new Monkey(true, 28, "Jonathan", 28, true);
+        Monkey mary = new Monkey(false, 29, "mary", 29, true);
+        Monkey leen = new Monkey(true, 27, "leen", 27, false);
+        Monkey martin = new Monkey(false, 26, "martin", 26, false);
 
-        animalShelter.addAnimal(monkey);
 
-        ByteArrayOutputStream outPut = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outPut));
+        animalShelter1.addAnimal(jon);
+        animalShelter1.addAnimal(mary);
+        animalShelter1.addAnimal(leen);
+        animalShelter1.addAnimal(martin);
 
-        animalShelter.treatAnimal(222);
 
-        String expectedOutput =
-                "Animal with number 28 is being treated.\n" +
-                "isClean=true, age=28  name='Jonathan', animalNumber=28\n";
+        animalShelter1.treatAnimal(3);
 
-        assertEquals(expectedOutput, outPut.toString());
+        // Controleer of het dier is behandeld
+        Animal treatedAnimal = animalShelter1.findAnimal(3);
+        boolean isCleanAfterTreatment = treatedAnimal != null && treatedAnimal.isClean();
+
+        // Assert
+        assertEquals(true, isCleanAfterTreatment, "Het dier met ANIMALNUMBER 3 moet zijn behandeld en schoon zijn.");
     }
+
 
     @Test
     void treatAnimalByNameTest() {
-        Animal jon = new Monkey(true, 28, "Jonathan", 28, true);
-        Animal leen = new Monkey(true, 27, "leen", 27, false);
+        Animal mary = new Monkey(true, 29, "mary", 3, false);
 
-        animalShelter.addAnimal(jon);
-        animalShelter.addAnimal(leen);
+
+        animalShelter.addAnimal(mary);
+
 
         ByteArrayOutputStream outPut = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outPut));
 
-        animalShelter.treatAnimal("Max");
+        animalShelter.treatAnimal("mary");
 
-        String expectedOutput = "Searching for animal with name : Max\n" +
-                "Animal found: isClean=true, age=28, name='Jonathan', animalNumber=28\n" +
-                "Animal with name Max is being treated.\n" +
-                "isClean=true, age=27, name='leen', animalNumber=27\n";
+        String expectedOutput =
+                "Animal found: isClean=true, age=29, name='mary', animalNumber=3\n" +
+                "Animal with name mary is being treated.\n";
 
-        assertEquals(expectedOutput, outPut.toString());
+
+        assertEquals(expectedOutput.trim(), outPut.toString().trim());
     }
 
     @Test
     void treatAllAnimalsTest() {
-        Animal dog1 = new Dog(false, 3, "Noel", 123, true);
-        Animal cat1 = new Cat(false, 3, "Mitzi", 345, true);
+        Animal leen = new Monkey(true, 27, "leen", 27, false);
 
-        animalShelter.addAnimal(dog1);
-        animalShelter.addAnimal(cat1);
+
+        animalShelter.addAnimal(leen);
+
 
         ByteArrayOutputStream outPut = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outPut));
 
         animalShelter.treatAllAnimal();
 
-        String expectedOutput = "isClean=true, age=5, name='Charlie', animalNumber=1\n" +
-                "isClean=true, age=3, name='Buddy', animalNumber=2\n";
+        String expectedOutput = "isClean=true, age=27, name='leen', animalNumber=27\n";
+        //String actualOutput = outPut.toString().trim();
+
         assertEquals(expectedOutput, outPut.toString());
+       // assertTrue(actualOutput.equalsIgnoreCase(expectedOutput));
     }
 
     @Test
     void findOldestAnimaTest() {
-        Animal dog = new Dog(false, 5, "Charlie", 1, true);
-        Animal cat = new Cat(false, 3, "Buddy", 2, false);
+        Animal monkeymary = new Monkey(false, 29, "mary", 29, true);
+        Animal monkeyjon = new Monkey(true, 28, "Jonathan", 28, true);
 
-        animalShelter.addAnimal(dog);
-        animalShelter.addAnimal(cat);
+        animalShelter.addAnimal(monkeymary);
+        animalShelter.addAnimal(monkeyjon);
 
-        assertEquals(dog, animalShelter.findOldestAnimal());
+        assertEquals(monkeymary, animalShelter.findOldestAnimal());
     }
 
     @Test
     void countAnimalsTest() {
-        Animal dog = new Dog(false, 5, "Charlie", 1, true);
-        Animal cat = new Cat(false, 3, "Buddy", 2, false);
+        Animal cat = new Cat(false, 7, "Nali", 798, true);
+        Animal dog = new Dog(false, 2, "Kobey", 33, true);
 
-        animalShelter.addAnimal(dog);
         animalShelter.addAnimal(cat);
+        animalShelter.addAnimal(dog);
 
         assertEquals(2, animalShelter.countAnimals());
     }
